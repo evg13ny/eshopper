f20cd8c3984e58e486bdfb300bfc6fe089286bb7
Evgeny Polyakov <evg_pol@yahoo.com>
2021-07-11 13:52:15 +0300

    added eshop_db table users

--- app/controllers/signup.php
+++ app/controllers/signup.php
@@ -5,6 +5,11 @@ class Signup extends Controller
     public function index()
     {
         $data['page_title'] = "Signup";
+
+        if ($_SERVER['REQUEST_METHOD'] == "POST") {
+            show($_POST);
+        }
+
         $this->view("signup", $data);
     }
 }
--- app/views/eshop/login.php
+++ app/views/eshop/login.php
@@ -1,10 +1,10 @@
 <?php $this->view("header", $data); ?>
 
-<section id="form">
+<section id="form" style="margin-top: 5px;">
 	<!--form-->
 	<div class="container">
-		<div class="row">
-			<div class="col-sm-4 col-sm-offset-1">
+		<div class="row" style="text-align: center;">
+			<div class="col-sm-4 col-sm-offset-1" style="float: none; display: inline-block">
 				<div class="login-form">
 					<!--login form-->
 					<h2>Login to your account</h2>
