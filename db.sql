5df4749ba4b3ad3cc84f2cf95659c692abbb8bcc
Evgeny Polyakov <evg_pol@yahoo.com>
2021-07-20 15:29:06 +0300

    add products table and category table in db

---
+++ db.sql
@@ -0,0 +1,141 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 20, 2021 at 02:27 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `eshop_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `category` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `user_url` varchar(60) NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `category` int(11) NOT NULL,
+  `price` double NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `image` varchar(500) NOT NULL,
+  `image2` varchar(500) NOT NULL,
+  `image3` varchar(500) NOT NULL,
+  `image4` varchar(500) NOT NULL,
+  `date` datetime NOT NULL,
+  `slag` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) NOT NULL,
+  `url_address` varchar(60) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `date` datetime NOT NULL,
+  `rank` varchar(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `url_address`, `name`, `email`, `password`, `date`, `rank`) VALUES
+(15, 'SSy0xP4JUO26Zl3oaOv', 'Sunny', 'sunny@mail.com', '7c4a8d09ca3762af61e59520943dc26494f8941b', '2021-07-16 12:11:35', 'admin'),
+(16, 'EHT29pvuDkIkV44NPprglYpyDXwA', 'Nik', 'nik@mail.com', 'dd5fef9c1c1da1394d6d34b248c51be2ad740840', '2021-07-16 13:49:14', 'customer'),
+(17, '3HwaDDlMbfbBT2NNO3afh3OeggOHbD0c6gRQnmW1pAUO6o', 'Mary', 'mary@email.com', '7c4a8d09ca3762af61e59520943dc26494f8941b', '2021-07-19 13:11:28', 'customer');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `category` (`category`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `slag` (`slag`),
+  ADD KEY `date` (`date`),
+  ADD KEY `price` (`price`),
+  ADD KEY `quantity` (`quantity`),
+  ADD KEY `category` (`category`),
+  ADD KEY `user_url` (`user_url`),
+  ADD KEY `description` (`description`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `url_address` (`url_address`),
+  ADD KEY `email` (`email`),
+  ADD KEY `name` (`name`),
+  ADD KEY `date` (`date`),
+  ADD KEY `rank` (`rank`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
