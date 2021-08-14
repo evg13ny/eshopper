908106d429ce84ed84e80c2191bed6f6f61c8ce3
Evgeny Polyakov <evg_pol@yahoo.com>
2021-08-14 23:08:50 +0300

    added cart

---
+++ app/controllers/cart.php
@@ -0,0 +1,53 @@
+<?php
+
+class Cart extends Controller
+{
+    public function index()
+    {
+        $User = $this->load_model('User');
+
+        $image_class = $this->load_model('Image');
+
+        $user_data = $User->check_login();
+
+        if (is_object($user_data)) {
+            $data['user_data'] = $user_data;
+        }
+
+        $DB = Database::newInstance();
+
+        $ROWS = false;
+
+        $prod_ids = array();
+
+        if (isset($_SESSION['CART'])) {
+            $prod_ids = array_column($_SESSION['CART'], 'id');
+            $ids_str = "'" . implode("','", $prod_ids) . "'";
+
+            $ROWS = $DB->read("select * from products where id in ($ids_str)");
+        }
+
+        if (is_array($ROWS)) {
+            foreach ($ROWS as $key => $row) {
+                foreach ($_SESSION['CART'] as $item) {
+                    if ($row->id == $item['id']) {
+                        $ROWS[$key]->cart_qty = $item['qty'];
+                        break;
+                    }
+                }
+            }
+        }
+
+        $data['page_title'] = "Cart";
+
+        if ($ROWS) {
+            foreach ($ROWS as $key => $row) {
+                $ROWS[$key]->image = $image_class->get_thumb_post($ROWS[$key]->image);
+            }
+        }
+
+        $data['ROWS'] = $ROWS;
+
+        $this->view("cart", $data);
+    }
+}
