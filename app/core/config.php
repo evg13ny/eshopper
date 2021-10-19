8e948a4f8e68c2979ad7d94123af3a75e04db996
Evgeny Polyakov <evg_pol@yahoo.com>
2021-10-19 13:17:26 +0300

    updated config db file

--- app/core/config.php
+++ app/core/config.php
@@ -2,11 +2,21 @@
 
 define("WEBSITE_TITLE", 'MY SHOP');
 
-define('DB_NAME', "eshop_db");
-define('DB_USER', "root");
-define('DB_PASS', "");
-define('DB_TYPE', "mysql");
-define('DB_HOST', "localhost");
+if ($_SERVER['SERVER_NAME'] == "localhost") {
+
+    define('DB_NAME', "eshop_db");
+    define('DB_USER', "root");
+    define('DB_PASS', "");
+    define('DB_TYPE', "mysql");
+    define('DB_HOST', "localhost");
+} else {
+
+    define('DB_NAME', "id17791162_eshop_db");
+    define('DB_USER', "id17791162_root");
+    define('DB_PASS', "!=U(F-B7?=Z|Eh99");
+    define('DB_TYPE', "mysql");
+    define('DB_HOST', "localhost");
+}
 
 define('THEME', 'eshop/');
 
