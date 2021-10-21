b9880cb3b28ee1643fc6a735d5a724f997b0ca26
Evgeny Polyakov <evg_pol@yahoo.com>
2021-10-21 12:42:08 +0300

    added paypal button

--- app/views/eshop/shop.php
+++ app/views/eshop/shop.php
@@ -38,4 +38,71 @@
 	</div>
 </section>
 
+<div id="smart-button-container">
+	<div style="text-align: center;">
+		<div id="paypal-button-container"></div>
+	</div>
+</div>
+<script src="https://www.paypal.com/sdk/js?client-id=AQHxY_ehAW56vlCCfTJN7gYlCAEyNgRTRqrTJQ7FPBQHKUcFDZKGUStHFgDgXR4euCk1wMI9afT5Y_rj&enable-funding=venmo&currency=RUB" data-sdk-integration-source="button-factory"></script>
+<script>
+	function initPayPalButton() {
+		paypal.Buttons({
+			style: {
+				shape: 'pill',
+				color: 'blue',
+				layout: 'vertical',
+				label: 'paypal',
+
+			},
+
+			createOrder: function(data, actions) {
+				return actions.order.create({
+					purchase_units: [{
+						"description": "Мой товар",
+						"amount": {
+							"currency_code": "RUB",
+							"value": 13.2,
+							"breakdown": {
+								"item_total": {
+									"currency_code": "RUB",
+									"value": 10
+								},
+								"shipping": {
+									"currency_code": "RUB",
+									"value": 3
+								},
+								"tax_total": {
+									"currency_code": "RUB",
+									"value": 0.2
+								}
+							}
+						}
+					}]
+				});
+			},
+
+			onApprove: function(data, actions) {
+				return actions.order.capture().then(function(orderData) {
+
+					// Full available details
+					console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+
+					// Show a success message within this page, e.g.
+					const element = document.getElementById('paypal-button-container');
+					element.innerHTML = '';
+					element.innerHTML = '<h3>Thank you for your payment!</h3>';
+
+					// Or go to another URL:  actions.redirect('thank_you.html');
+
+				});
+			},
+
+			onError: function(err) {
+				console.log(err);
+			}
+		}).render('#paypal-button-container');
+	}
+	initPayPalButton();
+</script>
+
 <?php $this->view("footer", $data); ?>
--- app/views/eshop/thanks.php
+++ app/views/eshop/thanks.php
@@ -2,6 +2,17 @@
 
 <?php $this->view("slider", $data); ?>
 
+<?php
+
+$str = '{"id":"WH-66445223J0522635N-3U789487FR927973U","event_version":"1.0","create_time":"2021-10-21T08:50:13.010Z","resource_type":"checkout-order","resource_version":"2.0","event_type":"CHECKOUT.ORDER.APPROVED","summary":"An order has been approved by buyer","resource":{"create_time":"2021-10-21T08:33:13Z","purchase_units":[{"reference_id":"default","amount":{"currency_code":"RUB","value":"13.20","breakdown":{"item_total":{"currency_code":"RUB","value":"10.00"},"shipping":{"currency_code":"RUB","value":"3.00"},"tax_total":{"currency_code":"RUB","value":"0.20"}}},"payee":{"email_address":"sb-yp7bd5780768@business.example.com","merchant_id":"EBJTVUSHVPBTN"},"description":"Мой товар","shipping":{"name":{"full_name":"John Doe"},"address":{"address_line_1":"25513540 River N343 W","admin_area_2":"Den Haag","admin_area_1":"2585","postal_code":"1015 CS","country_code":"NL"}}}],"links":[{"href":"https://api.sandbox.paypal.com/v2/checkout/orders/6MJ65684UE783060G","rel":"self","method":"GET"},{"href":"https://api.sandbox.paypal.com/v2/checkout/orders/6MJ65684UE783060G","rel":"update","method":"PATCH"},{"href":"https://api.sandbox.paypal.com/v2/checkout/orders/6MJ65684UE783060G/capture","rel":"capture","method":"POST"}],"id":"6MJ65684UE783060G","intent":"CAPTURE","payer":{"name":{"given_name":"John","surname":"Doe"},"email_address":"sb-i43m698192455@personal.example.com","payer_id":"KL82X6TRPKHDQ","address":{"country_code":"NL"}},"status":"APPROVED"},"links":[{"href":"https://api.sandbox.paypal.com/v1/notifications/webhooks-events/WH-66445223J0522635N-3U789487FR927973U","rel":"self","method":"GET"},{"href":"https://api.sandbox.paypal.com/v1/notifications/webhooks-events/WH-66445223J0522635N-3U789487FR927973U/resend","rel":"resend","method":"POST"}]}';
+echo "<pre>";
+$obj = json_decode($str);
+echo $obj->resource->purchase_units[0]->amount->value;
+echo "<br>";
+print_r($obj);
+
+?>
+
 <section>
 	<div class="container">
 		<div class="row">
